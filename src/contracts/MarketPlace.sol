0a7c885c7451ce10d65935cd406cd909847cb7f9
Taofeek <taofeekolamilekan218@gmail.com>
2021-12-10 10:59:15 +0100

    Initial commit

---
+++ src/contracts/MarketPlace.sol
@@ -0,0 +1,65 @@
+pragma solidity ^0.5.0;
+
+contract MarketPlace {
+
+    string public name;
+    uint public productCount = 0;
+
+    mapping(uint => Product) public products;
+    struct Product {
+        uint id;
+        uint price;
+        string name;
+        address payable owner;
+        bool purchased;
+    }
+
+    event ProductCreated(
+        uint id,
+        uint price,
+        string name,
+        address payable owner,
+        bool purchased
+    );
+
+    event ProductPurchased(
+        uint id,
+        uint price,
+        string name,
+        address payable owner,
+        bool purchased
+    );
+
+    constructor() public {
+        name = "Taoltech Coin Market Place";
+    }
+
+    function createProduct(string memory _name, uint _price) public {
+        require(bytes(_name).length > 0);
+        require(_price > 0);
+
+        productCount++;
+        products[productCount] = Product(productCount, _price, _name, msg.sender, false);
+
+        emit ProductCreated(productCount, _price, _name, msg.sender, false);
+    }
+
+    function buyProduct(uint _id) public payable {
+        Product memory _product = products[_id];
+        address payable _seller = _product.owner;
+
+        require(!_product.purchased);
+        require(_seller != msg.sender);
+        require(msg.value >= _product.price);
+        require(_product.id > 0 && _product.id <= productCount);
+
+        // Transfer ownership
+        _product.purchased = true;
+        _product.owner = msg.sender;
+
+        products[_id] = _product;
+        address(_seller).transfer(msg.value);
+
+        emit ProductPurchased(productCount, _product.price, _product.name, msg.sender, true);
+    }
+}
---
+++ test/MarketPlace.test.js
@@ -0,0 +1,65 @@
+const MarketPlace = artifacts.require('./MarketPlace.sol')
+require('chai').use(require('chai-as-promised')).should();
+
+contract('MarketPlace', ([deployer, buyer, seller]) => {
+    let marketPlace;
+
+    before(async () => {
+        marketPlace = await MarketPlace.deployed()
+    })
+
+    describe('deployment', async () => {
+
+        it('Deployed Successfully', async () => {
+            const address = await marketPlace.address;
+
+            assert.notEqual(address, 0x0);
+            assert.notEqual(address, '');
+            assert.notEqual(address, null);
+            assert.notEqual(address, undefined);
+        });
+
+        it('Has a name', async () => {
+            const name = await marketPlace.name();
+            assert.equal(name, 'Taoltech Coin Market Place');
+        });
+    })
+
+    describe('products', async () => {
+        let result, productCount;
+
+        before(async () => {
+            result = await marketPlace.createProduct('iPhone XR', web3.utils.toWei('1', 'Ether'));
+            productCount = await marketPlace.productCount();
+        })
+
+        it('creates product', async () => {
+            assert.equal(productCount, 1);
+            let event = result.logs[0].args;
+            assert.equal(event.id.toNumber(), productCount.toNumber(), 'ID is correct')
+
+            await marketPlace.createProduct('', web3.utils.toWei('1', 'Ether'), {from: seller}).should.be.rejected
+        });
+
+        it('sells product', async () => {
+            let oldBalance = await web3.eth.getBalance(seller);
+            oldBalance = new web3.utils.BN(oldBalance);
+
+            result = await marketPlace.buyProduct(productCount, {from: buyer, value: web3.utils.toWei('1', 'Ether')})
+            const event = result.logs[0].args;
+
+            assert.equal(event.name, 'iPhone XR', 'name is correct')
+            assert.equal(event.purchased, true, 'purchased is correct')
+            assert.equal(event.id.toNumber(), productCount.toNumber(), 'ID is correct');
+
+            let newBalance = await web3.eth.getBalance(seller);
+            newBalance = new web3.utils.BN(newBalance);
+
+            let price = web3.utils.toWei('1', 'Ether');
+            price = new web3.utils.BN(price);
+            const expectedBalance = oldBalance.add(price);
+
+            assert.equal(expectedBalance.toString(), newBalance.toString());
+        });
+    })
+})
